37691501c7e14b530c8bbfc295023bcf0b8f3021
i49 <julyborn715@gmail.com>
2017-01-19 22:17:43 +0900

    Implemented metadata loading.

--- komorebi/src/main/java/com/github/i49/komorebi/publication/MetadataLoader.java
+++ komorebi/src/main/java/com/github/i49/komorebi/publication/MetadataLoader.java
@@ -1,11 +1,38 @@
 package com.github.i49.komorebi.publication;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
 
 final class MetadataLoader {
 
 	public static Metadata load(InputStream s) {
 		Metadata m = new Metadata();
+		Yaml yaml = new Yaml();
+		Map<?, ?> map = yaml.loadAs(s, Map.class);
+		if (map != null) {
+			if (map.containsKey("title")) {
+				m.setTitle((String)map.get("title"));
+			}
+			if (map.containsKey("identifier")) {
+				m.setIdentifier((String)map.get("identifier"));
+			}
+			if (map.containsKey("language")) {
+				m.setLanguage(Locale.forLanguageTag((String)map.get("language")));
+			}
+			if (map.containsKey("publisher")) {
+				m.setPublisher((String)map.get("publisher"));
+			}
+			if (map.containsKey("creator")) {
+				List<?> creators = (List<?>)map.get("creator");
+				for (Object creator: creators) {
+					m.getCreators().add((String)creator);
+				}
+			}
+		}
 		return m;
 	}
 	
--- komorebi/src/test/java/com/github/i49/komorebi/publication/MetadataLoaderTest.java
+++ komorebi/src/test/java/com/github/i49/komorebi/publication/MetadataLoaderTest.java
@@ -1,6 +1,7 @@
 package com.github.i49.komorebi.publication;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,8 +12,14 @@ public class MetadataLoaderTest {
 
 	@Test
 	public void loadNormal() throws IOException {
-		try (InputStream s = getClass().getResourceAsStream("metatada.yml")) {
+		try (InputStream s = getClass().getResourceAsStream("metadata.yml")) {
 			Metadata m = MetadataLoader.load(s);
+			assertThat(m.getIdentifier(), equalTo("idpf.epub31.samples.moby-dick.xhtml"));
+			assertThat(m.getTitle(), equalTo("Moby-Dick"));
+			assertThat(m.getLanguage().toLanguageTag(), equalTo("en-US"));
+			assertThat(m.getPublisher(), equalTo("Harper & Brothers, Publishers"));
+			assertThat(m.getCreators().size(), equalTo(1));
+			assertThat(m.getCreators().get(0), equalTo("Herman Melville"));
 		}
 	}
 }
